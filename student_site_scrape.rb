afe2b7819041b6ad9af3e810713e318c371925ff
makafis <vinnyalfieri@gmail.com>
2015-06-10 14:15:31 -0400

    Added Student Class File

--- student_site_scrape.rb
+++ student_site_scrape.rb
@@ -1,21 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require_relative 'Student_Class.rb'
 
-
-class Student
-  attr_accessor :name, :excerpt, :student_link, :meta
-  @@student_hash = {}
-
-  def initialize(name)
-    @@student_hash[name.to_sym] = self
-    @name = name
-  end
-  #hash getter
-  def self.hash_getter
-    @@student_hash
-  end
-end #end class Student
 
 
 
